652f50d54d1a30a86a4a7b7a32b968416daad217
tuanlestudy <tuanlestudy@gmail.com>
2018-02-27 01:12:56 -0800

    image2gray
    -complete uglies function, but it doesnt work
    -pos-n-neg did not test

--- image2gray.py
+++ image2gray.py
@@ -13,17 +13,20 @@ def readFile(textPath):
     		dirPath=textPath[:i+1]
     		break
         i=i-1
-    f = file(textPath, "r")
-    line = f.readline()
-    while(len(line)):
+    try:
+        f = file(textPath, "r")
         line = f.readline()
-        temp=""+str(line)
-        temp2 = temp[:(len(temp)-1)]
-        imagePath = dirPath+temp2
-        print (imagePath)
-        image2gray(imagePath,dirPath,temp2)
-        count = count + 1
-        print (count)
+        while(len(line)):
+            line = f.readline()
+            temp=""+str(line)
+            temp2 = temp[:(len(temp)-1)]
+            imagePath = dirPath+temp2
+            print (imagePath)
+            image2gray(imagePath,dirPath,temp2)
+            count = count + 1
+            print (count)
+    except Exception as e:
+        print(str(e))
 
 def image2gray(imagePath,dirPath,nameimage):
     if not os.path.exists(dirPath+'neg'):
@@ -36,9 +39,60 @@ def image2gray(imagePath,dirPath,nameimage):
     except Exception as e:
         print(str(e))
 
+def find_uglies(textPath):
+    i=len(textPath)-1
+    while i > 0:
+    	if(textPath[i]=='/'):
+    		dirPath=textPath[:i+1]
+    		break
+        i=i-1
+    match = False
+    if not os.path.exists(dirPath+'uglies'):
+        os.makedirs(dirPath+'uglies')
+    for file_type in [dirPath+'neg']:
+        for img in os.listdir(file_type):
+            for ugly in os.listdir(dirPath+'uglies'):
+                try:
+                    current_image_path = dirPath+str(file_type)+'/'+str(img)
+                    #print(current_image_path)
+                    ugly = cv2.imread(dirPath+'uglies/'+str(ugly))
+                    question = cv2.imread(current_image_path)
+                    if ugly.shape == question.shape and not(np.bitwise_xor(ugly,question).any()):
+                        print('That is one ugly pic! Deleting!')
+                        print(current_image_path)
+                        #os.remove(current_image_path)
+                except Exception as e:
+                    print(str(e))
+
+
+def create_pos_n_neg(textPath):
+    i=len(textPath)-1
+    while i > 0:
+    	if(textPath[i]=='/'):
+    		dirPath=textPath[:i+1]
+    		break
+        i=i-1
+
+    for file_type in [dirPath+'neg']:
+        
+        for img in os.listdir(file_type):
+
+            if file_type == (dirPath+'pos'):
+                line = dirPath+file_type+'/'+img+' 1 0 0 50 50\n'
+                with open(dirPath+'info.dat','a') as f:
+                    f.write(line)
+            elif file_type == (dirPath+'neg'):
+                line = dirPath+file_type+'/'+img+'\n'
+                with open(dirPath+'bg.txt','a') as f:
+                    f.write(line)
+
 
 if __name__ == "__main__":
-	# Get user supplied values
-	textPath = sys.argv[1]
-	#textPath="data_pics/people/tokimngoc/raw/listpics.txt"
-	readFile(textPath)
+    # Get user supplied values
+    # #textPath = sys.argv[1]
+    textPath="data_pics/people/tokimngoc/raw/listpics.txt"
+    #readFile(textPath)
+    find_uglies(textPath)
+
+
+
